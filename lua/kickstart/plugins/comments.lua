745997d7c69641ffeb5d48718307848c62e2278a
Eric Jacquome <eric@ericjacquome.com>
2025-04-24 19:31:00 +0200

    added keybind docu for comments and changed completion keybinds.

--- lua/kickstart/plugins/comments.lua
+++ lua/kickstart/plugins/comments.lua
@@ -1,3 +1,24 @@
 return {
   'numToStr/Comment.nvim',
+  -- BASIC Mappings:
+  --
+  -- NORMAL MODE
+  -- `gcc` - Toggles the current line using linewise comment
+  -- `gbc` - Toggles the current line using blockwise comment
+  -- `[count]gcc` - Toggles the number of line given as a prefix-count using linewise
+  -- `[count]gbc` - Toggles the number of line given as a prefix-count using blockwise
+  -- `gc[count]{motion}` - (Op-pending) Toggles the region using linewise comment
+  -- `gb[count]{motion}` - (Op-pending) Toggles the region using blockwise comment
+  --
+  -- VISUAL MODE
+  -- `gc` - Toggles the region using linewise comment
+  -- `gb` - Toggles the region using blockwise comment
+  --
+  -- EXTRA Mappings:
+  --
+  -- NORMAL MODE
+
+  -- `gco` - Insert comment to the next line and enters INSERT mode
+  -- `gcO` - Insert comment to the previous line and enters INSERT mode
+  -- `gcA` - Insert comment to end of the current line and enters INSERT mode
 }
